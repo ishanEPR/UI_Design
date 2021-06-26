9e21767c611a4e1ac46e0708736e7500d401e98d
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-26 18:19:17 +0530

    created side bar

--- AdminPanel_ReactJS/admin_panel/src/components/sidebar/Sidebar.jsx
+++ AdminPanel_ReactJS/admin_panel/src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./sidebar.css";
-import {LineStyle,Timeline,TrendingUp} from "@material-ui/icons";
+import {LineStyle,Timeline,TrendingUp,Person, WebAsset,AttachMoney,BarChart,MailOutline,DynamicFeed,
+    ChatBubbleOutline,WorkOutline,Report} from "@material-ui/icons";
 
 export default function Sidebar() {
     return (
@@ -20,6 +21,84 @@ export default function Sidebar() {
                         <li className="sidebarListItem">
                             <TrendingUp className="sidebarIcon"/>
                             Sales
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+
+
+            <div className="sidebarWrapper">
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Quick Menu</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <Person className="sidebarIcon"/>
+                            Users
+                        </li>
+                        <li className="sidebarListItem">
+                            <WebAsset className="sidebarIcon"/>
+                            Products
+                        </li>
+                        <li className="sidebarListItem">
+                            <AttachMoney className="sidebarIcon"/>
+                            Transactions
+                        </li>
+
+                        <li className="sidebarListItem">
+                            <BarChart className="sidebarIcon"/>
+                            Reports
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+
+
+
+            <div className="sidebarWrapper">
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Notification</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <MailOutline className="sidebarIcon"/>
+                            Mail
+                        </li>
+                        <li className="sidebarListItem">
+                            <DynamicFeed className="sidebarIcon"/>
+                            Feedback
+                        </li>
+                        <li className="sidebarListItem">
+                            <ChatBubbleOutline className="sidebarIcon"/>
+                            Messages
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+
+
+            <div className="sidebarWrapper">
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Staff</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <WorkOutline className="sidebarIcon"/>
+                            Manage
+                        </li>
+                        <li className="sidebarListItem">
+                            <Timeline className="sidebarIcon"/>
+                            Analytics
+                        </li>
+                        <li className="sidebarListItem">
+                            <Report className="sidebarIcon"/>
+                            Reports
                         </li>
                     </ul>
 
