8b222c94d1120debe5669fb810cb7788ae650d5e
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-26 19:01:17 +0530

    created featured info

--- AdminPanel_ReactJS/admin_panel/src/App.js
+++ AdminPanel_ReactJS/admin_panel/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -9,9 +10,7 @@ function App() {
       <Topbar/>
       <div className="container">
         <Sidebar/>
-        <div className="others">
-        otherPages
-        </div>
+        <Home/>
       </div>
 
    
