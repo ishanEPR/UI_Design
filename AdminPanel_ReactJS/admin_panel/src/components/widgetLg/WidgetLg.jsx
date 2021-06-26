cfaec7f9b4e257f5521ebf15df445db122cbe2fd
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-27 02:13:11 +0530

    add widget

---
+++ AdminPanel_ReactJS/admin_panel/src/components/widgetLg/WidgetLg.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./widgetLg.css";
+
+export default function WidgetLg() {
+    return (
+        <div className="WidgetLg">
+            Large
+            
+        </div>
+    )
+}
---
+++ AdminPanel_ReactJS/admin_panel/src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./widgetSm.css";
+
+export default function WidgetSm() {
+    return (
+        <div className="WidgetSm">
+            small
+            
+        </div>
+    )
+}
