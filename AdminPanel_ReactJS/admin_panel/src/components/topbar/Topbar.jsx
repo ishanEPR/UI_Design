05e035fd9a0c6482dfceba8743bd8f2c3fa0412c
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-26 13:40:06 +0530

    add notification,settings..

--- AdminPanel_ReactJS/admin_panel/src/components/topbar/Topbar.jsx
+++ AdminPanel_ReactJS/admin_panel/src/components/topbar/Topbar.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import "./topbar.css";
-import {NotificationsNone} from '@material-ui/icons';
+import {NotificationsNone,Settings,Language} from '@material-ui/icons';
 
 export default function Topbar() {
     return (
     <div className="topbar">
         <div className="topbarWrapper">
             <div className="topLeft">
-               <span className="logo">
+               <span className="logo"> 
                    AdminPanel
                </span>
             </div>
             <div className="topRight">
-                <div className="topbarIcons">
-                <NotificationsNone/>
+                <div className="topbarIconContainer">
+                    <NotificationsNone/>
+                    <span className='topIconBadge'>2</span> 
+
+                </div>
+                <div className="topbarIconContainer">
+                    <Language/>
+                    <span className='topIconBadge'>2</span> 
+
+                </div>
+                <div className="topbarIconContainer">
+                    <Settings/>
+                    
 
                 </div>
             </div>
