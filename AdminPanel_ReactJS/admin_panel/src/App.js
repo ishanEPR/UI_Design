1578cff9ecfbad4b6ddbcc5d1c87e803ff940409
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-27 21:20:44 +0530

    operation create the update button

--- AdminPanel_ReactJS/admin_panel/src/App.js
+++ AdminPanel_ReactJS/admin_panel/src/App.js
@@ -4,6 +4,7 @@ import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 import {
   BrowserRouter as Router,
   Switch,
@@ -28,6 +29,12 @@ function App() {
 
             <Route path="/users">
               <UserList/>
+
+            </Route>
+
+
+            <Route path="/user/:userId">
+              <User/>
 
             </Route>
         </Switch>
