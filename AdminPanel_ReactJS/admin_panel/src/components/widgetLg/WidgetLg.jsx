e5939a5a2eb4a4c9e3d19aa8936a23146f63fba4
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-27 12:47:57 +0530

    add user details in large widget

--- AdminPanel_ReactJS/admin_panel/src/components/widgetLg/WidgetLg.jsx
+++ AdminPanel_ReactJS/admin_panel/src/components/widgetLg/WidgetLg.jsx
@@ -2,9 +2,78 @@ import React from 'react';
 import "./widgetLg.css";
 
 export default function WidgetLg() {
+
+    const Button= ({type}) =>{
+        return <button className={"WidgetLgButton " +type}>{type}</button>
+    }
     return (
         <div className="WidgetLg">
-            Large
+           <h3 className="widgetLgTitle">Latest transactions</h3>
+           <table className="WidgetLgTable">
+                <tr className="WidgetLgTr">
+                    <th className="WidgetLgTh">Customer</th>
+                    <th className="WidgetLgTh">Date</th>
+                    <th className="WidgetLgTh">Amount</th>
+                    <th className="WidgetLgTh">Status</th>
+
+                </tr>
+
+                <tr className="WidgetLgTr">
+                    <td className="WidgetLgUser">
+                        <img src="https://writestylesonline.com/wp-content/uploads/2021/02/Michele-Round-Circle-2020.png" alt="" className="widgetLgImg"/>
+                        <span className="widgetLgName">Ishan Reshmika</span>
+                    </td>
+                    <td className="WidgetLgDate">30 March 2021</td>
+                    <td className="WidgetLgAmount">$122.00</td>
+                    <td className="WidgetLgStatus">
+                        <Button type="Approved"/>
+                    </td>
+                   
+
+                </tr>
+
+                <tr className="WidgetLgTr">
+                    <td className="WidgetLgUser">
+                        <img src="https://writestylesonline.com/wp-content/uploads/2021/02/Michele-Round-Circle-2020.png" alt="" className="widgetLgImg"/>
+                        <span className="widgetLgName">Ishan Reshmika</span>
+                    </td>
+                    <td className="WidgetLgDate">30 March 2021</td>
+                    <td className="WidgetLgAmount">$122.00</td>
+                    <td className="WidgetLgStatus">
+                        <Button type="Declined"/>
+                    </td>
+                   
+
+                </tr>
+                <tr className="WidgetLgTr">
+                    <td className="WidgetLgUser">
+                        <img src="https://writestylesonline.com/wp-content/uploads/2021/02/Michele-Round-Circle-2020.png" alt="" className="widgetLgImg"/>
+                        <span className="widgetLgName">Ishan Reshmika</span>
+                    </td>
+                    <td className="WidgetLgDate">30 March 2021</td>
+                    <td className="WidgetLgAmount">$122.00</td>
+                    <td className="WidgetLgStatus">
+                        <Button type="Pending"/>
+                    </td>
+                   
+
+                </tr>
+                <tr className="WidgetLgTr">
+                    <td className="WidgetLgUser">
+                        <img src="https://writestylesonline.com/wp-content/uploads/2021/02/Michele-Round-Circle-2020.png" alt="" className="widgetLgImg"/>
+                        <span className="widgetLgName">Ishan Reshmika</span>
+                    </td>
+                    <td className="WidgetLgDate">30 March 2021</td>
+                    <td className="WidgetLgAmount">$122.00</td>
+                    <td className="WidgetLgStatus">
+                        <Button type="Approved"/>
+                    </td>
+                   
+
+                </tr>
+
+
+           </table>
             
         </div>
     )
