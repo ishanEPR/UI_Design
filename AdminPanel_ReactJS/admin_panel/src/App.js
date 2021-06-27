69d16eeb6d79c246ca772dfddb8ac4f420d6b9b8
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-27 18:26:04 +0530

    add router page in users page

--- AdminPanel_ReactJS/admin_panel/src/App.js
+++ AdminPanel_ReactJS/admin_panel/src/App.js
@@ -3,20 +3,42 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
+
+
 
 function App() {
   return (
+    <Router>
     <div>
       <Topbar/>
       <div className="container">
         <Sidebar/>
-        <Home/>
+        <Switch>
+            <Route exact path="/">
+              <Home/>
+
+            </Route>
+
+            <Route path="/users">
+              <UserList/>
+
+            </Route>
+        </Switch>
+        
       </div>
 
    
      
 
     </div>
+    </Router>
   );
 }
 
---
+++ AdminPanel_ReactJS/admin_panel/src/pages/userList/UserList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./userList.css";
+
+export default function UserList() {
+    return (
+        <div className="userList">
+        UserList
+            
+        </div>
+    )
+}
