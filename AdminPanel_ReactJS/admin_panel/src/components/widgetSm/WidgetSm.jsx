1dca963d355b75624155128512b68250d949bdbb
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-27 11:51:15 +0530

    small widget created and styled

--- AdminPanel_ReactJS/admin_panel/src/components/widgetSm/WidgetSm.jsx
+++ AdminPanel_ReactJS/admin_panel/src/components/widgetSm/WidgetSm.jsx
@@ -1,10 +1,87 @@
 import React from 'react';
 import "./widgetSm.css";
+import {Visibility} from '@material-ui/icons';
 
 export default function WidgetSm() {
     return (
         <div className="WidgetSm">
-            small
+            <span className="widgetSmTitle">New Join Members</span>
+            <ul className="widgetSmList">
+                <li className="widgetSmListItem">
+                    <img src="https://writestylesonline.com/wp-content/uploads/2021/02/Michele-Round-Circle-2020.png" alt="" className="widgetSmImg"/>
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Ishan Reshmika</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+
+                    </button>
+                </li>
+
+
+                <li className="widgetSmListItem">
+                    <img src="https://writestylesonline.com/wp-content/uploads/2021/02/Michele-Round-Circle-2020.png" alt="" className="widgetSmImg"/>
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Ishan Reshmika</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+
+                    </button>
+                </li>
+
+
+                <li className="widgetSmListItem">
+                    <img src="https://writestylesonline.com/wp-content/uploads/2021/02/Michele-Round-Circle-2020.png" alt="" className="widgetSmImg"/>
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Ishan Reshmika</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+
+                    </button>
+                </li>
+
+
+
+                <li className="widgetSmListItem">
+                    <img src="https://writestylesonline.com/wp-content/uploads/2021/02/Michele-Round-Circle-2020.png" alt="" className="widgetSmImg"/>
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Ishan Reshmika</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+
+                    </button>
+                </li>
+
+
+                <li className="widgetSmListItem">
+                    <img src="https://writestylesonline.com/wp-content/uploads/2021/02/Michele-Round-Circle-2020.png" alt="" className="widgetSmImg"/>
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Ishan Reshmika</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+
+                    </button>
+                </li>
+            </ul>
             
         </div>
     )
