5dd0b5ea2ff6c91b3e83197512ffb0b7e738eec3
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-28 11:03:19 +0530

    style product pages

--- AdminPanel_ReactJS/admin_panel/src/pages/product/Product.jsx
+++ AdminPanel_ReactJS/admin_panel/src/pages/product/Product.jsx
@@ -21,7 +21,41 @@ export default function Product() {
                 <div className="productTopLeft">
                     <Chart data={productData} dataKey="Sales" title="Sales Performance"/>
                 </div>
-                <div className="productTopRight"></div>
+                <div className="productTopRight">
+                    <div className="productInfoTop">
+                        <img src="https://images.unsplash.com/photo-1607203391514-b001be773a22?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXBwbGUlMjBwaG9uZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+                            alt=""
+                            className="productInfoImg"
+                        />
+                        <span className="productName">Apple Airpods</span>
+                    </div>
+                    <div className="productInfoBottom">
+                        <div className="productInfoItem">
+                            <span className="productInfokey">id</span>
+                            <span className="productInfoValue">123</span>
+
+                        </div>
+
+                        <div className="productInfoItem">
+                            <span className="productInfokey">Sales</span>
+                            <span className="productInfoValue">$123</span>
+
+                        </div>
+
+                        <div className="productInfoItem">
+                            <span className="productInfokey">active</span>
+                            <span className="productInfoValue">yes</span>
+
+                        </div>
+
+
+                        <div className="productInfoItem">
+                            <span className="productInfokey">in stock:</span>
+                            <span className="productInfoValue">no</span>
+
+                        </div>
+                    </div>
+                </div>
 
             </div>
             <div className="productBottom"></div>
